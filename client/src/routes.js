6db3d975851618e93ee5eb18591657d8e4a6e73e
kat09kat09 <kat09kat09@gmail.com>
2016-01-27 16:41:32 -0500

    Add route for /streamYourself

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import VideoPlayer from './containers/video-container';
+import ArtistContainer from './containers/artist-container'; 
 
 
 export default (
-  <Route path="/" component={ App } >
+  <div>
+    <Route path="/" component={ App } >
 
-    <IndexRoute component={VideoPlayer} />
+      <IndexRoute component={VideoPlayer} />
+      <Route path="streamYourself" component={ArtistContainer} />
 
-  </Route>
+    </Route>
+
+  </div>
+
 );
