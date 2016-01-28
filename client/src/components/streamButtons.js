3b8cc909451e1342d3826e2d306a0852cf73a4f8
kat09kat09 <kat09kat09@gmail.com>
2016-01-28 15:22:08 -0500

    Add class names

--- client/src/components/streamButtons.js
+++ client/src/components/streamButtons.js
@@ -3,9 +3,9 @@ import React from 'react';
 const StreamButtons = () => {
   return (
     <div className="streamButtons">
-      <button className="startBroadcast">Start Broadcast</button>
-      <button className="joinBroadcast">Watch the Stream!</button>
-      <button className="endBroadcast">Stop the Stream!</button>
+      <button className="startBroadcast fa fa-youtube-play">Start Stream</button>
+      <button className="joinBroadcast fa fa-eye">Watch Stream!</button>
+      <button className="endBroadcast fa fa-stop-circle">Stop Stream!</button>
     </div>
     )
 };
