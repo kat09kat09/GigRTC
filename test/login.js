a6abf1ccc9d97d8ff7ea0c91ec7d5f42fd880de1
Auggie <jhudak@alumni.berklee.edu>
2016-01-29 14:14:49 -0500

    (FEAT) supertests server

---
+++ test/login.js
@@ -0,0 +1,18 @@
+var superagent = require('superagent');
+var agent = superagent.agent();
+
+var theAccount = {
+  'userName': 'tds@tds.com',
+  'password': 'tds'
+};
+
+exports.login = function(request, done){
+      request
+        .post('/auth/getToken/')
+        .send(theAccount)
+        .end(function(err,res){
+          if(err) throw err;
+          agent.saveCookies(res)
+          done(agent)
+        })
+    };
--- test/test.js
+++ test/test.js
@@ -1,8 +1,15 @@
 var expect = require('chai').expect;
-var server = require('../server.js');
+var server = require('../server.js').server;
 var supertest = require('supertest');
+var login = require('./login');
 
-var request = supertest.agent(server);
+var request = supertest(server);
+
+var theAccount = {
+  'userName': 'tds@tds.com',
+  'password': 'tds'
+};
+
 
 describe("server", function(){
   
@@ -12,20 +19,51 @@ describe("server", function(){
     });
   });
 
-  xdescribe('GET root', function(){
+  describe('GET root', function(){
     it("should load the homepage", function(done){
       request
         .get('/')
-        .expect(200, function(err){
-          console.log(err);
-        }, done);
+        .expect(200, /<body/, done);
     });
   });
 
+  describe('Access should be restricted unless logged in', function(){
+    it('Should not have access to restricted routes', function(done){
+      request
+        .get('/getData/')
+        .expect(401, done);
+    });
+  });
+
+  describe('Should be able to log in', function(){
+    it('Should be able to log in', function(done){
+      request
+        .post('/auth/getToken/')
+        .send(theAccount)
+        .expect(200, done);
+    });
+  });
+  
 
 
+  describe('It should have access to restricted routes after login', function(){
+    var agent;
 
+    before(function(done){
+      login.login(request, function(loginAgent){
+        agent = loginAgent;
+        done();
+      });
+    });
 
+    
+    xit('should have restricted access after login', function(done){
+      var req = request.get('/getData/');
+      agent.attachCookies(req);
+      console.log(req);
+      req.expect(200, done);
+    });
 
+  });
 
 });
