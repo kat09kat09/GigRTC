951d4779d1fa4d27f5e58bf1c4a7f4bfcee98e3c
kat09kat09 <kat09kat09@gmail.com>
2016-02-03 17:27:15 -0500

    Add username to chat message display

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -121,7 +121,10 @@ class Chat extends Component {
 
 
   render() {
+
     const {messages, dispatch,userDetails, activeChannel}= this.props; 
+    console.log('this.props in Chat.js', this.props); 
+
     const filteredMessages = messages.filter(message => message.channelID === activeChannel);
     
     return (
@@ -173,6 +176,7 @@ class Chat extends Component {
 
 
 function mapStateToProps(state) {
+  console.log(state); 
   return {
       messages: state.messages.data,
       activeChannel: state.activeChannel.name,
