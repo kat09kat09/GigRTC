672290d6a8b33ee3a56d3ce182fa6cbe0cb9d1af
Auggie <jhudak@alumni.berklee.edu>
2016-02-02 19:52:00 -0500

    (FEAT) Adds skylink broadcaster page

--- client/src/containers/video-container.js
+++ client/src/containers/video-container.js
@@ -3,24 +3,48 @@ import StreamButtons from '../components/streamButtons';
 import {connect} from 'react-redux';
 import AuthenticateFacebook from '../components/auth/authenticatePage'
 
+
+
 export class VideoContainer extends Component {
 
-  componentWillMount(){
-      //var script   = document.createElement("script");
-      //script.type  = "text/javascript";
-      //script.src   = "../../public/js/webRTC.js";
-      //document.body.appendChild(script);
+  componentDidMount(){
+   skylink.on('mediaAccessSuccess', function(stream){
+    var vid = document.getElementById('video');
+    attachMediaStream(vid, stream);
+  });
+   
   }
-
   render () {
+    
     return (
       <div id="videoBig">
         <video id="video" autoPlay width="640px" height="480px" poster="public/img/guitarist.jpg"></video>
           <div className="video-overlay"></div>
-        <StreamButtons />
+        <StreamButtons startBroadcast={this.onBroadcast} isBroadcaster={true} endBroadcast={this.endBroadcast} currentRoom="gigg.tv"/>
+        {//isBroadcaster will be managed by state, is true when artist is logged in
+}
       </div>
     );
   }
+
+onBroadcast(){
+
+skylink.init({
+  apiKey: 'e8a678bc-e0e4-4605-aa76-cc857b7dbbd0',
+  defaultRoom: 'gigg.tv' //this will be managed by state at a later point
+}, () => {
+  skylink.joinRoom({
+    audio: true,
+    video: true
+  });
+});
+};
+
+endBroadcast(){
+  skylink.stopStream();
+  //Need to add poster when stram is stopped
+}
+
 }
 
 
