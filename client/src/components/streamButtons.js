2b4112a60b25a30fdffbd8a2bf77ea84f3e9bea0
Auggie <jhudak@alumni.berklee.edu>
2016-02-04 11:05:05 -0500

    (FEAT) Add ability to watch any artist stream

--- client/src/components/streamButtons.js
+++ client/src/components/streamButtons.js
@@ -5,7 +5,7 @@ const StreamButtons = (props) => {
   return (
     <div className="streamButtons">
       {(props.currentPrivelege && !props.watchMode) ? <button onClick={props.startBroadcast} className="startBroadcast fa fa-youtube-play">Start Stream</button> : ""}
-      {props.watchMode ? <button className="joinBroadcast fa fa-eye">Watch Stream!</button> : ""}
+      {props.watchMode ? <button onClick={props.startStream} className="startStream fa fa-eye">Watch Stream!</button> : ""}
       <button onClick={props.endBroadcast}className="endBroadcast fa fa-stop-circle">Stop Stream!</button>
 
     </div>
--- client/src/containers/watchAndHost/watchingHigherOrder.js
+++ client/src/containers/watchAndHost/watchingHigherOrder.js
@@ -28,6 +28,21 @@ export function videoHigherOrderFunction(Component) {
         });
 
     };
+    
+    function onStream(room){
+        skylink.init({
+            apiKey: CONFIG.SKYLINK_KEY,
+            defaultRoom: room
+        }, () => {
+            skylink.joinRoom({});
+        });
+
+        skylink.on('incomingStream', function(peerId, stream, isSelf){
+            if(isSelf) return;
+            var vid = document.getElementById('video');
+            attachMediaStream(vid, stream);
+        });
+    };
 
     function endBroadcast(){
         skylink.stopStream();
@@ -45,11 +60,19 @@ export function videoHigherOrderFunction(Component) {
             performanceActive({room:this.props.userDetails.user_name});
         }
 
+        onVideoStream(){
+            var room = window.location.href;
+            //NEEDS TO BE ALTERED FOR SERVER DON'T OVERLOOK
+            var re = /https\:\/\/localhost\:1338\/activeStream\//gi;//THIS NEEDS TO BE CHANGED TO WORK ON SERVER
+            onStream(room.replace(re,""));//this section needs the room of the clicked stream
+        }
+
         render () {
             return (
                 <div>
                     <Component startBroadcast={this.onVideoBroadCast.bind(this)}
                                endBroadcast={endBroadcast}
+                               startStream={this.onVideoStream.bind(this)}
                                currentPrivelege={this.props.userPrivelege}
                                watchMode={!!this.props.params.room}
                         {...this.state} {...this.props}/>
