882cbeb8600d9e02872b0dc03e042cce67f08224
Auggie <jhudak@alumni.berklee.edu>
2016-02-04 11:28:19 -0500

    (CLEANUP) Remove unnecessary comments from server

--- server.js
+++ server.js
@@ -252,20 +252,7 @@ app.get('/api/activeStreams',
         Performances.fetch().then(function(performances) {
             res.status(200).send(performances.models);
         });
-        // https://github.com/tgriesser/bookshelf/issues/629 possible issue with getting tags
-        // res send in new then?  .attach(what in here)
-        // Performance.fetchAll()
-        // .then(function(performances) {
-        //   return performances.each(function(performance) {
-        //     return performance.load(['tags']); // look this up
-        //   });
-        // })
-        // .then(function(performances) {
-        //   performances.at(0).related('tags').attach(FTW); // waht in attach ()?
-        // })
-        // .then(function(performances) {
-        //   res.send(200, performances.models); // ok to call models on fetchall result?
-        // });
+        
     });
 
 
