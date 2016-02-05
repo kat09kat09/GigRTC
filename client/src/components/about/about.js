4963f5bb6f0296211010483d3fa6baf37dc30c37
Auggie <jhudak@alumni.berklee.edu>
2016-02-04 21:55:59 -0500

    (FEAT) Link route to About

---
+++ client/src/components/about/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class About extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Team Dream Stream</h1>
+      </div>
+      );
+  }
+}
+
