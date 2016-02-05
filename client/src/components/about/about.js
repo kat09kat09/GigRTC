7d93028d419e7f8e551fa306f444ecf8e646a384
Auggie <jhudak@alumni.berklee.edu>
2016-02-05 10:03:31 -0500

    (FEAT) Add team descriptions

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -11,16 +11,23 @@ export default class About extends Component {
           <div className="avatars">
             <div className="venkatesh avatar">
               <Avatar src="../../../public/img/v.png" size="150"></Avatar>
+              <h2> Venkatesh Namburi </h2>
+              <h3> Scrum Master / Engineer </h3>           
             </div>
             <div className="auggie avatar">
-            <Avatar src="../../../public/img/a.png" size="150"></Avatar>
-
+              <Avatar src="../../../public/img/a.png" size="150"></Avatar>
+              <h2> Auggie (Jerome) Hudak </h2>
+              <h3> Product Owner / Engineer </h3>
             </div>
             <div className="madeline avatar">
-            <Avatar src="../../../public/img/m.png" size="150"></Avatar>
+              <Avatar src="../../../public/img/m.png" size="150"></Avatar>
+              <h2> Madeline Bernard </h2>
+              <h3> Engineer </h3>
             </div>
-            <div className="kathylyn avatar">
-            <Avatar src="../../../public/img/k.png" size="150"></Avatar>
+            <div className="kathlyn avatar">
+              <Avatar src="../../../public/img/k.png" size="150"></Avatar>
+              <h2> Kathlyn Ngo </h2>
+              <h3> Engineer </h3>
             </div>
           </div>
         </div>
