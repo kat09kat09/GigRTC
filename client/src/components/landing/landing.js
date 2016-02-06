54d9f53c3e6611877c722b88ce68071918519ce1
Auggie <jhudak@alumni.berklee.edu>
2016-02-06 12:01:04 -0500

    (FEAT) Add 2 CTA's to landing page

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
 
+const artistButton = {
+  'margin-top': "45px",
+  'margin-left': "35%",
+};
+
+const fanButton = {
+  'margin-top': '45px',
+  'margin-left': "10%",
+};
 
 export default class Landing extends Component {
   render(){
@@ -13,8 +23,10 @@ export default class Landing extends Component {
           <div className="left">
           </div>
           <div className="right">
-          <h1> Pick up the mic! </h1> 
-          <h1>Broadcast your GIGG now! </h1>
+            <h1> Pick up the mic! </h1> 
+            <h1>Broadcast your GIGG now! </h1>
+            <RaisedButton label="Artists" secondary={true} style={artistButton} />
+            <RaisedButton label="Fans" secondary={true} style={fanButton} />
           </div>
         </div>
       </div>
